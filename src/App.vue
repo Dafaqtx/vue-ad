63c9336715a62e83ac2a87819ce78cd3828b9405
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-08 23:49:13 +0600

    add structure for home component

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,9 @@
           @click="drawer = !drawer"
           class="hidden-md-and-up"
         ></v-toolbar-side-icon>
-        <v-toolbar-title>Vue ad</v-toolbar-title>
+        <v-toolbar-title>
+          <router-link to="/" tag="span" class="pointer">Vue ad</router-link>
+        </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn
@@ -64,3 +66,10 @@ export default {
   }
 }
 </script>
+
+<style scope>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
+
