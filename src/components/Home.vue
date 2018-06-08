83977732daa310b77166b8855d5a7608c61dd796
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-08 21:18:31 +0600

    set up app routing

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <v-layoout row>
+            <v-flex xs12>
+                <h1>Home</h1>
+            </v-flex>
+        </v-layoout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data () {
+        return {}
+    }
+}
+</script>
---
+++ src/components/User/Orders.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <v-layoout row>
+            <v-flex xs12>
+                <h1>Orders</h1>
+            </v-flex>
+        </v-layoout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data () {
+        return {}
+    }
+}
+</script>
