6d5f78a79d401f1b3c545a574a8674cba35cf76d
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-09 12:02:15 +0600

    set up new ad component

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,44 +1,44 @@
 <template>
-<v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-toolbar dark>
-                <v-toolbar-title>Login form</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-form v-model="valid" ref="form" validation>
-                  <v-text-field
-                    prepend-icon="person" 
-                    name="email" 
-                    label="Email" 
-                    type="email"
-                    v-model="email"
-                    :rules="emailRules"
-                  ></v-text-field>
-                  <v-text-field
-                    prepend-icon="lock" 
-                    name="password" 
-                    label="Password" 
-                    type="password"
-                    :counter="6"
-                    v-model="password"
-                    :rules="passwordRules"
-                  ></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                 color="primary"
-                 @click="onSubmit"
-                 :disabled="!valid"
-                >Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark>
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid" ref="form" validation>
+              <v-text-field
+                prepend-icon="person" 
+                name="email" 
+                label="Email" 
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock" 
+                name="password" 
+                label="Password" 
+                type="password"
+                :counter="6"
+                v-model="password"
+                :rules="passwordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="onSubmit"
+              :disabled="!valid"
+            >Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
