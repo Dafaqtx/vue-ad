a1d3ed193dc11ade9eb38ed3eccacf8ea4dfa102
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-09 12:27:23 +0600

    set up structure for ad component

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,10 +1,23 @@
 <template>
     <v-container>
-        <v-layoout row>
+        <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-card-media
+                      src="https://vuetifyjs.com/static/doc-images/carousel/sky.jpg"
+                      height="300px"
+                    ></v-card-media>
+                    <v-card-text>
+                        <h1 class="text--primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sint eius cum corporis. Neque quidem, enim ipsum, voluptates architecto soluta magnam dolores aut quis dolor hic tempore repellat! Corporis, sunt!</h1>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn flat class="warning">Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
-        </v-layoout>
+        </v-layout>
     </v-container>
 </template>
 
