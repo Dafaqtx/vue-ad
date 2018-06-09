d31a8bd008fe0fbf3140a2c1430881bd63fcd285
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-09 15:10:47 +0600

    add animation on login btn and redirect after succes login

--- src/store/user.js
+++ src/store/user.js
@@ -18,14 +18,26 @@ export default {
     async registerUser ({commit}, {email, password}) {
       commit('clearError')
       commit('setLoading', true)
-
       try {
         const user = await fb.auth().createUserWithEmailAndPassword(email, password)
         commit('setUser', new User(user.uid))
         commit('setLoading', false)
       } catch (error) {
         commit('setLoading', false)
-        commit('setErrors', error.message)
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async loginUser ({commit}, {email, password}) {
+      commit('clearError')
+      commit('setLoading', true)
+      try {
+        const user = await fb.auth().signInWithEmailAndPassword(email, password)
+        commit('setUser', new User(user.uid))
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
         throw error
       }
     }
